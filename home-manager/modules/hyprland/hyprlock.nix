1ec669a9f2582a1f25def7ea7c5012485573080d
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2024-12-16 01:39:03 +0300

    Add time.

--- home-manager/modules/hyprland/hyprlock.nix
+++ home-manager/modules/hyprland/hyprlock.nix
@@ -9,6 +9,18 @@
         no_fade_in = false;
       };
 
+      label = {
+        text = "$TIME";
+        font_size = 96;
+        font_family = "JetBrains Mono";
+        color = "rgba(235, 219, 178, 1.0)";
+        position = "0, 600";
+        halign = "center";
+        walign = "center";
+
+        shadow_passes = 1;
+      };
+
       background = [
         {
           path = "screenshot";
@@ -27,7 +39,7 @@
           inner_color = "rgb(40, 40, 40)";
           outer_color = "rgb(60, 56, 54)";
           outline_thickness = 5;
-          placeholder_text = "CGSG FOREVER!!!";
+          placeholder_text = "sussy baka";
           shadow_passes = 1;
         }
       ];
