4f8e53e14fb0a816e5204f81f1b789db7bcaf8f1
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2024-12-17 20:17:30 +0300

    Some extensions.

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -6,22 +6,40 @@
 
     userSettings = {
       "editor.formatOnSave" = true;
+      "editor.fontLigatures" = true;
+      "editor.tabSize" = 2;
+      "editor.insertSpaces" = true;
+      "editor.detectIndentation" = false;
+      "editor.minimap.scale" = 2;
       "nix.enableLanguageServer" = true;
       "nix.serverPath" = "nixd";
+      "workbench.activityBar.location" = "top";
     };
 
     extensions =
       with pkgs.vscode-marketplace;
       [
+        # Nix
         jnoortheen.nix-ide
         arrterian.nix-env-selector
+
+        # C++
         xaver.clang-format
         twxs.cmake
+
+        # Utility
+        alefragnani.bookmarks
+        cweijan.vscode-office
+
+        # Typst
+        mgt19937.typst-preview
+        surv.typst-math
       ]
       ++ (
         with import <unstable> { };
         with pkgs.vscode-extensions;
         [
+          # C++
           ms-vscode.cpptools
         ]
       );
