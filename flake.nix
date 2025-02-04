579bea5a054895a44309b958cd5a6b91d451fc2d
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2025-02-04 23:12:27 +0300

    Update flake.

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/release-24.11";
 
     nvf = {
       url = "github:notashelf/nvf";
