6b6c73dbbd9dedb71fb6b466b743e086a052d9ba
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2025-02-23 17:10:48 +0300

    Nvidia update.

--- hosts/fx51/modules/nvidia.nix
+++ hosts/fx51/modules/nvidia.nix
@@ -1,17 +1,55 @@
 {
+  config,
+  lib,
+  ...
+}: {
   services.xserver.videoDrivers = ["nvidia"];
 
+  # hardware.graphics.enable = true;
+
   hardware.nvidia = {
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+
     modesetting.enable = true;
-    open = true;
+
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+
+    dynamicBoost.enable = true;
+
+    nvidiaSettings = true;
 
     prime = {
       offload = {
         enable = true;
         enableOffloadCmd = true;
       };
+      sync.enable = lib.mkDefault false;
+
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
   };
+
+  #specialisation = {
+  #  graphics.configuration = {
+  #    system.nixos.tags = ["graphics"];
+  #    hardware.nvidia = {
+  #      powerManagement = {
+  #        finegrained = lib.mkForce false;
+  #      };
+
+  #      prime = {
+  #        offload = {
+  #          enable = lib.mkForce false;
+  #          enableOffloadCmd = lib.mkForce false;
+  #        };
+  #        sync.enable = lib.mkForce true;
+  #      };
+  #    };
+  #  };
+  #};
 }
