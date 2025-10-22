5d78db821f4b9347a10eb9cfb9366ec08d952090
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2025-10-22 23:37:22 +0300

    Add haskell and rust.

--- modules/home/cli/helix.nix
+++ modules/home/cli/helix.nix
@@ -11,6 +11,9 @@
   config = lib.mkIf config.cli.helix.enable {
     programs.helix = {
       enable = true;
+
+      package = pkgs.helix;
+
       settings = {
         editor = {
           line-number = "relative";
@@ -18,30 +21,76 @@
           auto-completion = true;
           auto-format = true;
           auto-info = true;
+          idle-timeout = 0;
           cursor-shape = {
             normal = "block";
             insert = "bar";
             select = "underline";
           };
+          lsp = {
+            display-inlay-hints = true;
+            display-messages = true;
+          };
+          indent-guides = {
+            render = true;
+          };
+        };
+        keys.normal = {
+          space.space = "file_picker";
+          space.w = ":w";
+          space.q = ":q";
         };
       };
+
       languages.language = [
         {
           name = "nix";
-          scope = "source.nix";
-          file-types = ["nix"];
+          language-servers = ["nixd"];
           auto-format = true;
-          comment-token = "#";
-          block-comment-tokens = {
-            start = "/*";
-            end = "*/";
-          };
           formatter = {
             command = "${pkgs.alejandra}/bin/alejandra";
             args = ["--quiet"];
           };
         }
+        {
+          name = "rust";
+          language-servers = ["rust-analyzer"];
+          auto-format = true;
+          formatter = {
+            command = "rustfmt";
+          };
+        }
+        {
+          name = "haskell";
+          language-servers = ["haskell-language-server"];
+          auto-format = true;
+          formatter = {
+            command = "stylish-haskell";
+          };
+        }
       ];
+
+      language-server = {
+        rust-analyzer = {
+          command = "rust-analyzer";
+          config.check = {
+            command = "clippy";
+          };
+        };
+        nixd = {
+          command = "${pkgs.nixd}/bin/nixd";
+        };
+        haskell-language-server = {
+          command = "haskell-language-server";
+          args = ["--lsp"];
+          config = {
+            haskell = {
+              formattingProvider = "stylish-haskell";
+              checkProject = true;
+            };
+          };
+        };
+      };
     };
   };
 }
