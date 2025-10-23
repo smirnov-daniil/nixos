3d8b2ea4d040b496dcb1048c2d4a1fed2db8cb9d
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2025-10-23 21:16:39 +0300

    Fix language-server location.

--- modules/home/cli/helix.nix
+++ modules/home/cli/helix.nix
@@ -42,51 +42,53 @@
         };
       };
 
-      languages.language = [
-        {
-          name = "nix";
-          language-servers = ["nixd"];
-          auto-format = true;
-          formatter = {
-            command = "${pkgs.alejandra}/bin/alejandra";
-            args = ["--quiet"];
-          };
-        }
-        {
-          name = "rust";
-          language-servers = ["rust-analyzer"];
-          auto-format = true;
-          formatter = {
-            command = "rustfmt";
-          };
-        }
-        {
-          name = "haskell";
-          language-servers = ["haskell-language-server"];
-          auto-format = true;
-          formatter = {
-            command = "stylish-haskell";
-          };
-        }
-      ];
+      languages = {
+        language = [
+          {
+            name = "nix";
+            language-servers = ["nixd"];
+            auto-format = true;
+            formatter = {
+              command = "${pkgs.alejandra}/bin/alejandra";
+              args = ["--quiet"];
+            };
+          }
+          {
+            name = "rust";
+            language-servers = ["rust-analyzer"];
+            auto-format = true;
+            formatter = {
+              command = "rustfmt";
+            };
+          }
+          {
+            name = "haskell";
+            language-servers = ["haskell-language-server"];
+            auto-format = true;
+            formatter = {
+              command = "stylish-haskell";
+            };
+          }
+        ];
 
-      language-server = {
-        rust-analyzer = {
-          command = "rust-analyzer";
-          config.check = {
-            command = "clippy";
+        language-server = {
+          rust-analyzer = {
+            command = "rust-analyzer";
+            config.check = {
+              command = "clippy";
+            };
           };
-        };
-        nixd = {
-          command = "${pkgs.nixd}/bin/nixd";
-        };
-        haskell-language-server = {
-          command = "haskell-language-server";
-          args = ["--lsp"];
-          config = {
-            haskell = {
-              formattingProvider = "stylish-haskell";
-              checkProject = true;
+          nixd = {
+            command = "${pkgs.nixd}/bin/nixd";
+          };
+          haskell-language-server = {
+            command = "haskell-language-server";
+            args = ["--lsp"];
+            config = {
+              haskell = {
+                formattingProvider = "stylish-haskell";
+                checkProject = true;
+              };
             };
           };
         };
