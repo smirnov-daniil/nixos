e267cea0a04e25ae616d32d5a97f6cf48c1d0f6e
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2025-12-13 17:09:31 +0300

    fix.

--- modules/home/desktop/hypr/windowrules.nix
+++ modules/home/desktop/hypr/windowrules.nix
@@ -12,11 +12,11 @@
         "move 990 60,size 900 170,pin,noinitialfocus,class:(showmethekey-gtk)"
         "noborder,nofocus,class:(showmethekey-gtk)"
 
-        "workspace 1,title:(zen)"
-        "workspace 2,title:^(.*tty.*)$"
-        "workspace 3,title:^(.*[T|t]elegram.*)$"
-        "workspace 4,class:(code)"
-        "workspace 5,title:(com.obsproject.Studio)"
+        "workspace 1,class:^(.*zen.*)$"
+        "workspace 2,class:^(.*tty.*)$"
+        "workspace 3,class:^(.*telegram.*)$"
+        "workspace 4,class:^(.*code.*)$"
+        "workspace 5,class:^(.*obs.*)$"
 
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
