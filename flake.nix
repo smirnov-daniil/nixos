11e1ce53adb7f19b038681574e86e79394d73c16
Daniil Smirnov <dsmirnov.ds2+github@yandex.com>
2026-09-16 23:36:20 +0300

    chore(tai-lung): track skinem master and pin the chat title change

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Private source: fetched by the invoking user's SSH agent, never at runtime.
-    skinem.url = "git+ssh://git@github.com/smirnov-daniil/tributum.git?rev=e273f5f911840d94d7291b2a259865e2fe84d730";
+    skinem.url = "git+ssh://git@github.com/smirnov-daniil/tributum.git?ref=master";
     flake-parts.url = "github:hercules-ci/flake-parts";
     deploy-rs = {
       url = "github:serokell/deploy-rs";
